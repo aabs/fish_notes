6264c3332309b2690d33a02bf8b777906c0dd52f
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-07-24 15:01:26 +1000

    bug fish + avoid deprecated fn

--- functions/__fd2_n2_create.fish
+++ functions/__fd2_n2_create.fish
@@ -1,5 +1,5 @@
 function _fd2_n2_note_create -a title -d "create a new text note"
-  set escaped_file_name (_escape_string $title)
+  set escaped_file_name (__fd2_to_slug $title)
   set d (date --iso-8601)
   set p "$fd2_notes_home/$d-$escaped_file_name.md"
   _fd2_n2_note_edit $p
