bfb160032572c7c0f465a0d132f90109e33fa743
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-08-05 06:33:21 +1000

    WIP

--- functions/__fd2_n2_create.fish
+++ functions/__fd2_n2_create.fish
@@ -1,20 +1,19 @@
 function _fd2_n2_note_create -d "create a new text note"
--			set -l title ''
-  
-      trace fd2_define_command title >&2
-  
-      getopts argv | while read -l key value
-          switch key
-              case t title
-                  set title value
-          end
-      end
+  set -l title ''
 
-      if test -z $title
-        error 'title must be set (use the -p option)' >&2
-        return 1
+  getopts argv | while read -l key value
+      switch key
+          case t title
+              set title value
       end
+  end
 
+  if test -z $title
+    error 'title must be set (use the -t option)' >&2
+    return 1
+  end
+
+  trace fd2_define_command title >&2
   set escaped_file_name (fd2_to_slug $title)
   set d (date --iso-8601)
   set p "$fd2_notes_home/$d-$escaped_file_name.md"
@@ -22,3 +21,4 @@ function _fd2_n2_note_create -d "create a new text note"
   debug wrote "$p" to notes
 end
 
+# vim: set et foldlevelstart=-1 foldmethod=manual sw=2 ts=2 tw=0 : 
