4bfa74832aacf68144dfc427247a75a99ba1fc75
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-08-05 15:32:31 +1000

    simplifying string handling for pcreate title

--- functions/__fd2_n2_pcreate.fish
+++ functions/__fd2_n2_pcreate.fish
@@ -1,16 +1,9 @@
-# Defined in /tmp/fish.AKVfgu/__fd2_n2_pcreate.fish @ line 2
+# Defined in /tmp/fish.4BaA3y/__fd2_n2_pcreate.fish @ line 2
 function __fd2_n2_pcreate --description 'create a new text note within a project area'
-	set -l title ''
-
-  getopts $argv | while read -l key value
-      switch $key
-          case t title
-              set title $value
-      end
-  end
+	set -l title "$argv"
 
   if test -z $title
-    error 'title must be set (use the -t option)' >&2
+    error 'title must be provided' >&2
     return 1
   end
   
