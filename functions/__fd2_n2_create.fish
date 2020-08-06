25293c0b7f9f781147b378989d3c626d91f54ba3
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-08-07 08:44:40 +1000

    using fish-get for gathering title etc

--- functions/__fd2_n2_create.fish
+++ functions/__fd2_n2_create.fish
@@ -1,24 +1,21 @@
-function _fd2_n2_note_create -d "create a new text note"
+function __fd2_n2_create -d "create a new text note"
   set -l title ''
+  set -l tags ''
 
-  getopts argv | while read -l key value
-      switch key
-          case t title
-              set title value
-      end
-  end
-
+  set -l title (get -p "what is the title?\n>")
   if test -z $title
-    error 'title must be set (use the -t option)' >&2
+    error "Can't proceed without a title! Aborting" >&2
     return 1
   end
 
-  trace fd2_define_command title >&2
+  set -l tags (get -p "what [optional] tags describe this note?\n[]>")
+
   set escaped_file_name (fd2_to_slug $title)
   set d (date --iso-8601)
   set p "$fd2_notes_home/$d-$escaped_file_name.md"
+  echo -e "# $title\n\n<!-- tags: $tags -->\n\n" > $p
+  echo -e "\n\n<!-- vim: set et foldlevelstart=20 foldmethod=syntax sw=2 ts=2 tw=72 : -->" >> $p
   __fd2_n2_edit $p
-  debug wrote "$p" to notes
 end
 
 # vim: set et foldlevelstart=-1 foldmethod=manual sw=2 ts=2 tw=0 : 
