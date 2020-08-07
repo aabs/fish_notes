89792f644f7bfacbd0394451f2686dadd792fa4f
Andrew Matthews <andrew.matthews@team.telstra.com>
2020-08-07 10:10:54 +1000

    simplified text input even further

--- functions/__fd2_n2_create.fish
+++ functions/__fd2_n2_create.fish
@@ -1,16 +1,17 @@
-function __fd2_n2_create -d "create a new text note"
-  set -l title ''
+# Defined in /tmp/fish.Bq2heX/__fd2_n2_create.fish @ line 2
+function __fd2_n2_create --description 'create a new text note'
+	set -l title ''
   set -l tags ''
 
-  fd2_get "What is the title?"
-  set -l title $fd2_input
+  echo "What is the title?"
+  set -l title (read)
   if test -z $title
     error "Can't proceed without a title! Aborting" >&2
     return 1
   end
 
-  fd2_get  "what [optional] tags describe this note?"
-  set -l tags $fd2_input
+  echo  "what [optional] tags describe this note?"
+  set -l tags (read)
 
   set escaped_file_name (fd2_to_slug $title)
   set d (date --iso-8601)
@@ -19,5 +20,3 @@ function __fd2_n2_create -d "create a new text note"
   echo -e "\n\n<!-- vim: set et foldlevelstart=20 foldmethod=syntax sw=2 ts=2 tw=72 : -->" >> $p
   __fd2_n2_edit $p
 end
-
-# vim: set et foldlevelstart=-1 foldmethod=manual sw=2 ts=2 tw=0 : 
